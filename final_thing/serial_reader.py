63285b7ce1b46d21f08f280b5121cfb0bcff8b48
keijoraamat <raamtkeijo@gmail.com>
2024-05-06 08:26:37 +0300

    serial reader quite ok

--- final_thing/serial_reader.py
+++ final_thing/serial_reader.py
@@ -1,7 +1,7 @@
 import csv
-import pyserial as serial
+import serial
 
-port = 'COM3'
+port = 'COM5'
 baudrate = 115200
 out_filename = 'output.csv'
 # Open the serial port (replace '/dev/ttyUSB0' with your port path)
@@ -15,5 +15,6 @@ with open(out_filename, 'w', newline='') as file:
     while True:
         line = ser.readline().decode('utf-8').strip()  # Read a line from the serial port
         if line:  # If the line is not empty
-            data = line.split(',')  # Split the line into a list (replace ',' with your delimiter)
-            writer.writerow(data)  # Write the data to the CSV file
+            timestamp, spl= line.split(';')  # Split the line into a list (replace ',' with your delimiter)
+            print(f"time: {timestamp}, SPL: {spl}")
+            writer.writerow([timestamp,spl])  # Write the data to the CSV file
